ac1404d2d4f1c3f47d66fe469230d48449518083
Peally <pieclandre@gmail.com>
2023-03-13 18:40:47 +0100

    feature : create and display avatar and name as a link

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,34 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+const photographerFactory = (data) => {
+  const { name, portrait, id, country, city, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `./assets/photographers/Photographers_ID_Photos/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
-}
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+
+    // Create a link with img and name
+
+    const link = document.createElement("a");
+    link.setAttribute("href", `photographer.html?id=${id}`);
+    link.setAttribute("alt", name);
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", "");
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    link.appendChild(img);
+    link.appendChild(h2);
+
+    article.appendChild(link);
+
+    return article;
+  }
+
+  return {
+    name,
+    picture,
+    getUserCardDOM,
+  };
+};
+
+export { photographerFactory };
