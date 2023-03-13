1e520abd9797d8181f493c70c7f61178c04fca42
Peally <pieclandre@gmail.com>
2023-03-13 18:46:49 +0100

    feat : Retrieve current URL id

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,6 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import { photographerFactory } from "../factories/photographer.js";
+
+//Retrieve current URL id
+
+const url = new URL(window.location);
+const id = parseInt(url.searchParams.get("id"));
