5da36cb20e2b487346d38f8fe2ece5e4a768acb4
Peally <pieclandre@gmail.com>
2023-03-14 20:05:37 +0100

    feat : create all mediaCard in the factory

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,14 +1,42 @@
-
-const mediaFactory = (data) => {
-
-
+const mediaFactory = (data, photographerName) => {
+  const heart = document.createElement("i");
+  heart.setAttribute("class", "fa-solid fa-heart");
   function getTotalLike() {
-      let totalLikes = 0
-    data.map(el => totalLikes += el.likes)
-    return totalLikes
+    let totalLikes = 0;
+    data.map((el) => (totalLikes += el.likes));
+    return totalLikes;
   }
 
-  return { getTotalLike };
+  function getMediaCardDOM() {
+    let { id, photographerId, title, image, video, likes, date, price } = data;
+
+    const mediaCardDOM = document.createElement("article");
+    const img = document.createElement("img");
+    console.log(image);
+    img.setAttribute(
+      "src",
+      `./assets/photographers/${photographerName.replace(" ", "_")}/${
+        image ? image : video.replace(".mp4", ".jpg")
+      }`
+    );
+    img.setAttribute("alt", title);
+    const text = document.createElement("div");
+    const name = document.createElement("p");
+    name.innerText = title;
+    const like = document.createElement("p");
+    like.innerText = likes;
+    like.appendChild(heart);
+
+    text.appendChild(name);
+    text.appendChild(like);
+
+    mediaCardDOM.appendChild(img);
+    mediaCardDOM.appendChild(text);
+
+    return mediaCardDOM;
+  }
+
+  return { getTotalLike, getMediaCardDOM };
 };
 
-export {mediaFactory};
+export { mediaFactory };
