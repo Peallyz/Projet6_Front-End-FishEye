671590783772636062fdc1e7a48689c141347b07
pierre <pieclandre@gmail.com>
2023-03-20 13:19:25 +0100

    feat : display none pages to let the modal only visible

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,17 @@
 function displayModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "block";
+  const header = document.querySelector(".main_header")
+  const main = document.querySelector("#main")
+  header.style.display ="none"
+  main.style.display ="none"
 }
 
 function closeModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "none";
+  const header = document.querySelector(".main_header")
+  const main = document.querySelector("#main")
+  header.style.display ="block"
+  main.style.display ="block"
 }
