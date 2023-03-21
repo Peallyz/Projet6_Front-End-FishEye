e78d5af5f313439b65c3c6fbbaafe98f005c1572
Peally <pieclandre@gmail.com>
2023-03-21 22:44:23 +0100

    fix : switch style with js into add and remove class d-none

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,17 +1,18 @@
 function displayModal() {
-  const modal = document.getElementById("contact_modal");
-  modal.style.display = "block";
-  const header = document.querySelector(".main_header")
-  const main = document.querySelector("#main")
-  header.style.display ="none"
-  main.style.display ="none"
+  const modal = document.querySelector(".contact_modal");
+  modal.classList.remove("close");
+  const header = document.querySelector(".main_header");
+  const main = document.querySelector("#main");
+  header.classList.add("close");
+  main.classList.add("close");
 }
 
 function closeModal() {
-  const modal = document.getElementById("contact_modal");
+  const modal = document.querySelector(".contact_modal");
+  modal.classList.remove("close");
   modal.style.display = "none";
-  const header = document.querySelector(".main_header")
-  const main = document.querySelector("#main")
-  header.style.display ="block"
-  main.style.display ="block"
+  const header = document.querySelector(".main_header");
+  const main = document.querySelector("#main");
+  header.classList.remove("close");
+  main.classList.remove("close");
 }
