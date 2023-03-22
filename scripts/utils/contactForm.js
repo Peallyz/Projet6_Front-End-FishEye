be49638d032fd66b6c133f20c35991f5795e5e5f
Peally <pieclandre@gmail.com>
2023-03-22 20:38:30 +0100

    fix : clean code to fit the model

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,18 +1,9 @@
 function displayModal() {
   const modal = document.querySelector(".contact_modal");
   modal.classList.remove("close");
-  const header = document.querySelector(".main_header");
-  const main = document.querySelector("#main");
-  header.classList.add("close");
-  main.classList.add("close");
 }
 
 function closeModal() {
   const modal = document.querySelector(".contact_modal");
-  modal.classList.remove("close");
-  modal.style.display = "none";
-  const header = document.querySelector(".main_header");
-  const main = document.querySelector("#main");
-  header.classList.remove("close");
-  main.classList.remove("close");
+  modal.classList.add("close");
 }
