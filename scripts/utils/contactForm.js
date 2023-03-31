71d256f51bc27a23009909a5bee39e07cbecd55b
Peally <pieclandre@gmail.com>
2023-03-31 19:37:38 +0200

    feat : add auto focus on first input when opening form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,8 @@
 function displayModal() {
   const modal = document.querySelector(".contact_modal");
+  const prenomInput = document.querySelector("#prenom");
   modal.classList.remove("close");
+  prenomInput.focus();
 }
 
 function closeModal() {
