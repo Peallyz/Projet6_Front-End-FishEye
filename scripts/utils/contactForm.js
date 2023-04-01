2f692d3246638bc37d6d2056e4f16c6ac37d71d0
Peally <pieclandre@gmail.com>
2023-04-01 20:57:07 +0200

    feat : handle aria-hidden on form open/close

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,11 +1,24 @@
 function displayModal() {
   const modal = document.querySelector(".contact_modal");
-  const prenomInput = document.querySelector("#prenom");
+  modal.setAttribute("aria-hidden", "false");
   modal.classList.remove("close");
+
+  const header = document.querySelector(".main_header");
+  header.setAttribute("aria-hidden", "true");
+  const main = document.querySelector("#main");
+  main.setAttribute("aria-hidden", "true");
+
+  const prenomInput = document.querySelector("#prenom");
   prenomInput.focus();
 }
 
 function closeModal() {
   const modal = document.querySelector(".contact_modal");
+  modal.setAttribute("aria-hidden", "true");
   modal.classList.add("close");
+
+  const header = document.querySelector(".main_header");
+  header.setAttribute("aria-hidden", "false");
+  const main = document.querySelector("#main");
+  main.setAttribute("aria-hidden", "false");
 }
