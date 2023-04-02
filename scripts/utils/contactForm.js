28b42084ec8ab20ecc509db57cc0a9f72034eb8d
Peally <pieclandre@gmail.com>
2023-04-02 16:14:01 +0200

    feat : send form and handle background interactivity on form open

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,4 +1,4 @@
-function displayModal() {
+const displayModal = () => {
   const modal = document.querySelector(".contact_modal");
   modal.setAttribute("aria-hidden", "false");
   modal.classList.remove("close");
@@ -10,9 +10,10 @@ function displayModal() {
 
   const prenomInput = document.querySelector("#prenom");
   prenomInput.focus();
-}
+  handleBackgroundInteractivity(true);
+};
 
-function closeModal() {
+const closeModal = () => {
   const modal = document.querySelector(".contact_modal");
   modal.setAttribute("aria-hidden", "true");
   modal.classList.add("close");
@@ -21,4 +22,47 @@ function closeModal() {
   header.setAttribute("aria-hidden", "false");
   const main = document.querySelector("#main");
   main.setAttribute("aria-hidden", "false");
-}
+
+  handleBackgroundInteractivity(false);
+
+  document.querySelector(".photograph-header .contact_button").focus();
+};
+
+const handleBackgroundInteractivity = (formOpen) => {
+  const targetatbleElement = [];
+
+  targetatbleElement.push(document.querySelector(".main_header a"));
+  targetatbleElement.push(
+    document.querySelector(".photograph-header .contact_button")
+  );
+  targetatbleElement.push(document.querySelector("#selector"));
+  targetatbleElement.push(...document.querySelectorAll(".sort ul li a"));
+  targetatbleElement.push(...document.querySelectorAll(".medias article img"));
+  targetatbleElement.push(...document.querySelectorAll(".medias article p i"));
+
+  if (formOpen === true) {
+    targetatbleElement.forEach((element) =>
+      element.setAttribute("tabindex", "-1")
+    );
+  } else if (formOpen === false) {
+    targetatbleElement.forEach((element) =>
+      element.setAttribute("tabindex", "1")
+    );
+  }
+};
+
+/////////////////////SEND FORM////////////////////////
+
+const sendForm = (e) => {
+  e.preventDefault();
+  const form = document.querySelector(".modal form");
+  const formData = new FormData(form);
+  for (let [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
+  const modal = document.querySelector(".contact_modal");
+  modal.classList.add("close");
+  closeModal();
+};
+
+export { sendForm, closeModal, displayModal };
